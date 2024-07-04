def1c3e902c7e511202c299dfef07529d221e31b
alexmrtc <alexmrtc@gmail.com>
2024-07-04 12:49:52 +0200

    Create basic CRUD for FireAlert

--- Controllers/FireAlertController.cs
+++ Controllers/FireAlertController.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using FireAlert.Data;
 using FireAlert.DTOs;
+using FireAlert.DTOs.Create;
+using FireAlert.DTOs.Update;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +30,51 @@ namespace FireAlert.Controllers
             var fireAlertDto = _mapper.Map<List<FireAlertDto>>(fireAlerts);
 
             return Ok(fireAlertDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FireAlertDto>> CreateFireAlert(CreateFireAlertDto createFireAlertDto)
+        {
+            var fireAlert = _mapper.Map<Models.FireAlert>(createFireAlertDto);
+            _context.Add(fireAlert);
+            await _context.SaveChangesAsync();
+
+            return Ok(fireAlert);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateFireAlert(int id, [FromBody] UpdateFireAlertDto updateFireAlertDto)
+        {
+            var doesExist = await _context.FireAlerts.AnyAsync(fireAlert => fireAlert.Id == id);
+
+            if (!doesExist)
+            {
+                return NotFound();
+            }
+
+            var fireAlertData = _mapper.Map<Models.FireAlert>(updateFireAlertDto);
+            fireAlertData.Id = id;
+
+            _context.Update(fireAlertData);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteFireAlert(int id)
+        {
+            var fireAlert = await _context.FireAlerts.FindAsync(id);
+
+            if (fireAlert == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(fireAlert);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
         }
     }
 }
